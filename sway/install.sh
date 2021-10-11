3888d7d46a5427b3ad75c56497ec5082d47c5a66
gmorer <gmorer@pm.me>
2021-10-12 01:54:38 +0200

    sway: fix mouse delay(remove auto lock)

--- sway/install.sh
+++ sway/install.sh
@@ -4,5 +4,7 @@ rm -rf ${HOME}/.config/sway
 ln -fs  ${PWD}/config/sway ${HOME}/.config/sway
 rm -rf ${HOME}/.config/waybar
 ln -fs  ${PWD}/config/waybar ${HOME}/.config/waybar
-rm -rf ${HOME}/.config/rofi
-ln -fs  ${PWD}/config/rofi ${HOME}/.config/rofi
+rm -rf ${HOME}/.config/wofi
+ln -fs  ${PWD}/config/wofi ${HOME}/.config/wofi
+rm -rf ${HOME}/.config/kanshi
+ln -fs  ${PWD}/config/kanshi ${HOME}/.config/kanshi
