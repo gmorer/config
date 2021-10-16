81f8da40f128dad284f7fbd17b1ab2675a419a54
gmorer <gmorer@pm.me>
2021-10-17 01:18:06 +0200

    nvim: tmp freeze fix

--- nvim/resources/lua/config/cmp.lua
+++ nvim/resources/lua/config/cmp.lua
@@ -4,35 +4,38 @@ vim.opt.completeopt = "menuone,noselect"
 
 cmp.setup({
     snippet = {
-      expand = function(args)
-         require("luasnip").lsp_expand(args.body)
-      end,
-   },
+       expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+       end,
+    },
     formatting = {
-      format = function(entry, vim_item)
-         -- load lspkind icons
-         vim_item.kind = string.format(
-            "%s %s",
-            require("config.lspkind_icons").icons[vim_item.kind],
-            vim_item.kind
-         )
+       format = function(entry, vim_item)
+          -- load lspkind icons
+          vim_item.kind = string.format(
+             "%s %s",
+             require("config.lspkind_icons").icons[vim_item.kind],
+             vim_item.kind
+          )
 
-         vim_item.menu = ({
-            nvim_lsp = "[LSP]",
-            nvim_lua = "[Lua]",
-            buffer = "[BUF]",
-         })[entry.source.name]
+          vim_item.menu = ({
+             nvim_lsp = "[LSP]",
+             nvim_lua = "[Lua]",
+             buffer = "[BUF]",
+          })[entry.source.name]
 
-         return vim_item
-      end,
-	},
+          return vim_item
+       end,
+ 	},
 	mapping = {
-		['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
+		-- Waiting for fix
+		-- ['<Tab>'] = cmp.mapping.select_next_item(),
+		-- ['<S-Tab>'] = cmp.mapping.select_prev_item()
+		['<Up>'] = cmp.mapping.select_next_item(),
+		['<Down>'] = cmp.mapping.select_prev_item()
 	},
     sources = {
-      { name = "nvim_lsp" },
-      { name = "buffer" },
-      { name = "nvim_lua" },
-   },
+       { name = "nvim_lsp" },
+       { name = "buffer" },
+       { name = "nvim_lua" },
+    },
 })
