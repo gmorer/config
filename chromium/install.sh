66bba0651af95f2af69bb0900ba1ec1f94b778ae
Guilhem <guilhem@trail.gg>
2022-07-01 09:14:27 +0200

    sway: office changes

--- chromium/install.sh
+++ chromium/install.sh
@@ -2,12 +2,12 @@
 
 rm -rf cache \
 && mkdir -p cache \
-&& mkdir -p ${HOME}/bin \
+&& mkdir -p ${HOME}/.local/bin \
 && cd cache \
 && JSON=`curl -s https://api.github.com/repos/macchrome/linchrome/releases/latest` \
 && URL=`echo ${JSON} | jq -r '.assets[].browser_download_url' | grep AppImage` \
 && NAME=`echo ${JSON} | jq -r '.assets[].name' | grep AppImage` \
 && wget -q ${URL} \
 && chmod +x ${NAME} \
-&& ln -fs "${PWD}/${NAME}" "${HOME}/bin/chromium"
+&& ln -fs "${PWD}/${NAME}" "${HOME}/.local/bin/chromium"
 
