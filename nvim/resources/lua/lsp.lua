e37127683f3b0bf9a1155b8ec689ae507998bfe8
tet <gmorer@pm.me>
2023-02-13 20:30:41 +0100

    nvim: new lua lsp

--- nvim/resources/lua/lsp.lua
+++ nvim/resources/lua/lsp.lua
@@ -170,14 +170,27 @@ local servers = {
     ocamllsp = {},
     gopls = {},
     pyright = {settings = {python = {formatting = {provider = 'yapf'}}}},
-    sumneko_lua = {
+    lua_ls =  {
       settings = {
         Lua = {
-          completion = {
-            callSnippet = "Replace",
-          }
-        }
-      }
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
     },
     texlab = {
         settings = {
