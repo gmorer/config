bddc8326afa5cbce8a65deeb30ea58df8044f34e
tet <gmorer@pm.me>
2023-02-22 21:25:18 +0000

    default .zshrc

--- dev/init.sh
+++ dev/init.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+echo '
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+export PATH="/workdir/.cargo/bin:${PATH}"
+' > ~/.zshrc
 
+echo ${ZSHRC} > ~/.zshrc
 # This has to be run only once to initialise the user
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH="/workdir/.cargo/bin:${PATH}"
 rustup component add rust-src
 rustup component add rust-analyzer
