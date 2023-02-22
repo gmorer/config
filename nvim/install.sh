44943c2f6fd6a0fc8668bebc2b4f769c4cb88e36
tet <gmorer@pm.me>
2023-02-22 19:47:44 +0000

    nvim: remove packer from nvim install

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# https://github.com/wbthomason/packer.nvim/issues/198
-# Async install of the packages is not working in headless mod ATM
-# Y ou should run :PackerSync once in nvim
-
 echo "Depend on go rust clang, nice nerd fonts..."
 
 mkdir -p ${HOME}/.local/bin \
 && curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ${HOME}/.local/bin/nvim \
 && chmod +x ${HOME}/.local/bin/nvim \
-&& git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim \
-&& mkdir -p ${HOME}/.local/share/nvim/site/pack/packer/start/ \
 && rm -rf ${HOME}/.config/nvim \
 && ln -fs  ${PWD}/resources ${HOME}/.config/nvim
