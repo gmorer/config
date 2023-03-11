cdd4f78f013bf09d34d6d259ea01293980c4e3ce
tet <gmorer@pm.me>
2023-03-11 23:37:48 +0100

    dev: run wayland app

--- dev/init.sh
+++ dev/init.sh
@@ -5,6 +5,8 @@ source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 export PATH="/workdir/.cargo/bin:${PATH}"
 ' > ~/.zshrc
 
+mkdir /workdir/.xdg_runtime_dir
+
 echo ${ZSHRC} > ~/.zshrc
 # This has to be run only once to initialise the user
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
