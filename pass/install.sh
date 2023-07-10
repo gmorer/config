b7383b904228e3d0d07c93766a76e13e854ddf7d
Guilhem <guilhem@trail.gg>
2023-07-10 11:39:32 +0200

    pass: install instruction

--- pass/install.sh
+++ pass/install.sh
@@ -2,6 +2,14 @@
 
 ln -fs ${HOME}/Sync/metadata/pass ${HOME}/.password-store
 
+
+# Register new key
+# $> gpg-connect-agent "scd serialno" "learn --force" /bye
+#
+# Trust the key
+# $> gpg --edit-key
+# gpg> trust
+
 #unset SSH_AGENT_PID
 #if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
 #	export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
