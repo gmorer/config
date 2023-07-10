a62f8bdcda42a0f70967426d73f80c84086239c4
Guilhem <guilhem@trail.gg>
2023-07-10 18:08:06 +0200

    dev: fix init script

--- dev/init.sh
+++ dev/init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+set -x
+
 echo '
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
@@ -10,5 +13,6 @@ mkdir /workdir/.xdg_runtime_dir
 echo ${ZSHRC} > ~/.zshrc
 # This has to be run only once to initialise the user
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
 rustup component add rust-src
 rustup component add rust-analyzer
